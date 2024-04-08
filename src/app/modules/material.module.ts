92eb9638ae534f391894d3a2d5ad93169fa9b56e
Asher Contreras <asher.contreras1480@stu.mtec.edu>
2024-04-08 08:24:12 -0600

    finished with unit 1-6

---
+++ src/app/modules/material.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatListModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatListModule
+  ]
+})
+export class MaterialModule { }
