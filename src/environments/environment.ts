58adddcecb652d2f706de52546c83bddda795779
Asher Contreras <asher.contreras1480@stu.mtec.edu>
2024-04-23 10:25:50 -0600

    set up firebase, unit 14 complete

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyCs1ZXU-PGt5shc2vYsgMg8C1HgG27uHx4",
+        authDomain: "timesheet-13841.firebaseapp.com",
+        projectId: "timesheet-13841",
+        storageBucket: "timesheet-13841.appspot.com",
+        messagingSenderId: "637805732369",
+        appId: "1:637805732369:web:4db994205287462af107b0",
+        measurementId: "G-NF6HVWN35F"
+    },
+  };
